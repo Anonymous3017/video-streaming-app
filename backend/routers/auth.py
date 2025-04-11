ab7e0a94c6aad814a86d55543a1e989660faa5a2
ayush-inzint <ayush@inzint.com>
2025-04-11 23:49:39 +0530

    Enhance authentication flow by adding response handling and cookie management for login and refresh token endpoints

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -2,7 +2,7 @@ from db.db import get_db
 from db.models.user import User
 from helper.auth_helper import get_secret_hash
 from pydantic_models.auth_models import ConfirmSignupRequest, LoginRequest, SignupRequest
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response, Cookie
 import boto3
 from secret_keys import SecretKeys
 from sqlalchemy.orm import Session
@@ -59,7 +59,7 @@ def signup_user(data: SignupRequest, db: Session = Depends(get_db),):
 
 
 @router.post("/login")
-def login_user(data: LoginRequest):
+def login_user(data: LoginRequest, response: Response):
 
     try:
         secret_hash = get_secret_hash(data.email, COGNITO_CLIENT_ID, COGNITO_CLIENT_SECRET)
@@ -72,7 +72,30 @@ def login_user(data: LoginRequest):
                 "PASSWORD": data.password,
                 "SECRET_HASH": secret_hash, 
             }
-        )        
+        )
+
+        auth_result = cognito_response.get("AuthenticationResult")
+
+        if not auth_result:
+            return HTTPException(
+                400, "Cognito did not return a valid authentication result")
+        
+        access_token = auth_result.get("AccessToken")
+        refresh_token = auth_result.get("RefreshToken")
+
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+        )
+
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            secure=True,
+        )
 
         return {"message":"User Logged In successfully"}
     except Exception as e:
@@ -95,6 +118,42 @@ def confirm_signup(data: ConfirmSignupRequest):
         )        
 
         return {"message": "User confirmed successfully"}
+    except Exception as e:
+        raise HTTPException(
+            400, f'Cognito signup exception: {e}')
+
+
+
+@router.post("/refresh")
+def refresh_token(refresh_token: str = Cookie(None),
+                  user_cognito_sub: str = Cookie(None),
+                   response: Response = None):
+
+    try:
+        secret_hash = get_secret_hash(user_cognito_sub, COGNITO_CLIENT_ID, COGNITO_CLIENT_SECRET)
+
+        cognito_response = cognito_client.initiate_auth(
+            ClientId=COGNITO_CLIENT_ID,
+            AuthFlow="REFRESH_TOKEN_AUTH",
+            AuthParameters={'REFRESH_TOKEN': refresh_token, 'SECRET_HASH': secret_hash},
+        )   
+
+        auth_result = cognito_response.get("AuthenticationResult")
+
+        if not auth_result:
+            return HTTPException(
+                400, "Cognito did not return a valid authentication result")
+        
+        access_token = auth_result.get("AccessToken")
+
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+        )
+
+        return {"message":"Access token refreshed successfully"}
     except Exception as e:
         raise HTTPException(
             400, f'Cognito signup exception: {e}')
