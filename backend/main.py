4971ce8b862209f5afb6cf707a2354a6d99ca9ad
ayush-inzint <ayush@inzint.com>
2025-04-11 15:14:18 +0530

    Add database setup and user model with Cognito integration
    - Create base and db modules for SQLAlchemy setup
    - Define User model with cognito_sub field
    - Implement get_db function for session management
    - Update signup_user function to save user data in the database
    - Add POSTGRES_DB_URL to SecretKeys for database connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
+from db.base import Base
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import auth
+from db.db import engine
 
 app = FastAPI()
 
@@ -19,3 +21,5 @@ app.include_router(auth.router, prefix="/auth", tags=["auth"])
 @app.get("/")
 def root():
     return "Hello, World!!!!"
+
+Base.metadata.create_all(bind=engine)
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,8 +1,11 @@
+from db.db import get_db
+from db.models.user import User
 from helper.auth_helper import get_secret_hash
 from pydantic_models.auth_models import SignupRequest
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 import boto3
 from secret_keys import SecretKeys
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 sectet_keys = SecretKeys()
@@ -15,19 +18,40 @@ REGION_NAME = sectet_keys.REGION_NAME
 cognito_client = boto3.client("cognito-idp", region_name="ap-south-1")
 
 @router.post("/signup")
-def signup_user(data: SignupRequest):
+def signup_user(data: SignupRequest, db: Session = Depends(get_db),):
 
-    secret_hash = get_secret_hash(data.email, COGNITO_CLIENT_ID, COGNITO_CLIENT_SECRET)
+    try:
+        secret_hash = get_secret_hash(data.email, COGNITO_CLIENT_ID, COGNITO_CLIENT_SECRET)
 
-    cognito_response = cognito_client.sign_up(
-        ClientId=COGNITO_CLIENT_ID,
-        Username=data.email,
-        Password=data.password,
-        SecretHash=secret_hash,
-        UserAttributes=[
-            {"Name": "email", "Value": data.email},
-            {"Name": "name", "Value": data.name},
-        ],
+        cognito_response = cognito_client.sign_up(
+            ClientId=COGNITO_CLIENT_ID,
+            Username=data.email,
+            Password=data.password,
+            SecretHash=secret_hash,
+            UserAttributes=[
+                {"Name": "email", "Value": data.email},
+                {"Name": "name", "Value": data.name},
+            ],
+            
+        )
+
+        cognito_sub = cognito_response.get("UserSub")
+
+        if not cognito_sub:
+            return HTTPException(
+                400, "Cognito did not return a valid sub")
         
-    )
-    return {"msg": "User signed up successfully, Please Verify your email if required "}
+        new_user = User(
+            name=data.name,
+            email=data.email,
+            cognito_sub=cognito_sub,
+        )
+
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        return {"msg": "User signed up successfully, Please Verify your email if required "}
+    except Exception as e:
+        raise HTTPException(
+            400, f'Cognito signup exception: {e}')
