91cafe2299c1f802c2669dc11ef9c234cb607277
ayush-inzint <ayush@inzint.com>
2025-04-15 18:48:58 +0530

    Add Redis integration for video caching and update docker-compose for Redis service

--- backend/routers/video.py
+++ backend/routers/video.py
@@ -1,9 +1,11 @@
+import json
 from db.models.video import ProcessingStatus, Video, VisibilityStatus
 from fastapi import APIRouter, Depends, HTTPException
 from db.db import get_db
 from db.middleware.auth_middleware import get_current_user
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
+from db.redis_db import redis_client
 
 
 router = APIRouter()
@@ -31,16 +33,29 @@ def get_video_info(
     user=Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
+    cache_key = f"video:{video_id}"
+    cached_data = redis_client.get(cache_key)
+
+    if cached_data:
+        print("Cache hit")
+        print(cached_data)
+        return json.loads(cached_data)
     video = (
         db.query(Video)
         .filter(
             Video.id == video_id,
-            # Video.is_processing == ProcessingStatus.COMPLETED,
+            Video.is_processing == ProcessingStatus.COMPLETED,
             or_(
                 Video.visibility == VisibilityStatus.PUBLIC,
                 Video.visibility == VisibilityStatus.UNLISTED,
             ),
         )
         .first()
+    )
+
+    redis_client.setex(
+        cache_key,
+        3600,
+        json.dumps(video.to_dict()),
     )
     return video
