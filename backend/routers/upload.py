fbd4c486e804ddfca10725b418bc38513de5a0f1
ayush-inzint <ayush@inzint.com>
2025-05-17 18:55:20 +0530

    fix: Remove extra space in thumbnail ID generation for presigned URL

--- backend/routers/upload.py
+++ backend/routers/upload.py
@@ -47,7 +47,7 @@ def get_presigned_url(user=Depends(get_current_user)):
 @router.get("/url/thumbnail")
 def get_presigned_url_thumbnail(thumbnail_id: str, user=Depends(get_current_user)):
     try:
-        thumbnail_id = thumbnail_id.replace("videos/", "thumbnails/").replace(".mp4", " ")
+        thumbnail_id = thumbnail_id.replace("videos/", "thumbnails/").replace(".mp4", "")
         print(thumbnail_id)
         response = s3_client.generate_presigned_url(
             "put_object",
